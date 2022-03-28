36f189427e1b2b07aa9a52245224e9b43768f642
Chitchaya Chimtanoo <cc.chitchaya@gmail.com>
2022-03-28 00:07:54 -0500

    complete speech modules

--- backend/app.py
+++ backend/app.py
@@ -17,13 +17,8 @@ def root():
 
 
 @app.post('/transcribe')
-# def transcribe(audio_content: str):
-#     return speech_to_text.transcribe(audio_content)
-def transcribe():
-    with open('/Users/top34051/Desktop/2022/projects/parrot-ai/output.mp3', 'rb') as f:
-        content = f.read()
-    print(speech_to_text.transcribe(content))
-
+def transcribe(audio_content: str):
+    return speech_to_text.transcribe(audio_content)
 
 
 @app.post('/forms')
