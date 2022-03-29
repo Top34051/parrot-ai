37377d830725dfb9a68f9d582cb691ddf3873e55
Varich <thapecroth@gmail.com>
2022-03-28 18:09:06 -0700

    1. add code to make voice recording work

---
+++ frontend/hooks/use-recordings-list.ts
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import { deleteAudio } from "../handlers/recordings-list";
+import { Audio } from "../types/recorder";
+import { v4 as uuid } from "uuid";
+
+export default function useRecordingsList(audio: string | null) {
+  const [recordings, setRecordings] = useState<Audio[]>([]);
+
+  useEffect(() => {
+    if (audio)
+      setRecordings((prevState: Audio[]) => {
+        return [...prevState, { key: uuid(), audio }];
+      });
+  }, [audio]);
+
+  return {
+    recordings,
+    deleteAudio: (audioKey: string) => deleteAudio(audioKey, setRecordings),
+  };
+}
